de5776272e98be63661722a2eeba89b75aa8e765
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-30 13:41:21 -0300

    update

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,5 @@
+// import nome from '../../utils/api'
+// import nome from '../../../../../../x/a/d/f/b/f/api'
+// import nome from 'utils/api'
+
+// console.log(nome)
---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const nome = 'Paulinha'
+
+export const getJobs = () => {
+
+}
+
+
+
+
+
+
+
+
+//getJobs().then(r => console.log(r))
