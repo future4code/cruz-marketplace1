f496e9f49d4888b5337741c32d6433ca447015f0
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-30 17:16:00 -0300

    teste api

--- src/pages/professional.jsx
+++ src/pages/professional.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import { getAllJobs, takeJob, giveUpJob } from 'utils/api'
 
 export class Professional extends Component {
+  state = {
+    jobs: [],
+    isLoading: true
+  }
+  
+  componentDidMount () {
+    getAllJobs().then(r => {
+      this.setState({ jobs: [...r.jobs], isLoading: false })
+    })
+  }
+
   render() {
     return (
       <div>
@@ -8,6 +20,17 @@ export class Professional extends Component {
         <button onClick={() => this.props.changePage('home')}>
           voltar para home
         </button>
+        <ul>
+          {this.state.jobs.map(job => (
+            <li>
+              {job.title}
+              {job.description}
+              {job.value}
+              {job.taken}
+              {job.paymentMethods}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
