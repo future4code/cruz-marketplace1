0ea15132bed8f3a1decba429b96faefbec26c4fb
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-03-31 09:48:12 -0300

    ajustes no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import React, { Component } from 'react'
 import { Home, User, Professional, CreateJob, NotFound } from 'pages'
 import Container from '@material-ui/core/Container'
-import styled from 'styled-components'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import * as api from 'utils/api'
-import RegistrationPage from 'components/RegistrationPage'
 
 class App extends Component {
-  // state = { page: 'home'}
+  state = { page: 'home'}
   
-  // componentDidMount () {
-  //   api.getAllJobs()
-  //   .then(r => console.log(r))
-  // }
+  componentDidMount () {
+    api.getAllJobs()
+    .then(r => console.log(r))
+  }
   
-  // changePage = page => this.setState({ page })
+  changePage = page => this.setState({ page })
   
-  // renderPage = () => {
-  //   switch (this.state.page) {
-  //     case 'home': return Home
-  //     case 'user': return User
-  //     case 'professional': return Professional
-  //     case 'create-job': return CreateJob
-  //     default: return NotFound
-  //   }
+  renderPage = () => {
+    switch (this.state.page) {
+      case 'home': return Home
+      case 'user': return User
+      case 'professional': return Professional
+      case 'create-job': return CreateJob
+      default: return NotFound
+    }
 
-  // }
+  }
   
 
 	render () {
-    // const SelectedPage = this.renderPage()
+    const SelectedPage = this.renderPage()
 
     return (
       <Container>
-        {/* <CssBaseline />
+        <CssBaseline />
         <select name="page" id="page"
           onChange={(e) => this.setState({ page: e.target.value })}
         >
@@ -44,8 +42,7 @@ class App extends Component {
           <option value="notfound">NotFound</option>
         </select>
 
-        <SelectedPage changePage={this.changePage} /> */}
-        <RegistrationPage/>
+        <SelectedPage changePage={this.changePage} />
       </Container>
     )
     }
