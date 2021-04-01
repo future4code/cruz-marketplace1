06084bfa4c580f76de6e9dd81e44928a7bf586aa
KiwiDev808 <lucasbacela@hotmail.com>
2021-04-01 15:24:21 -0300

    Criacao pasta da pag do user

--- src/pages/User/user.jsx
+++ src/pages/User/user.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { getAllJobs, deleteJob } from "../utils/api";
+import { getAllJobs, deleteJob } from "../../utils/api";
 import { Button } from "@material-ui/core";
-import logo from "../images/LogoComNome.svg";
+import logo from "images/LogoComNome.svg";
+import {PageContainer, MainContent, Header, SectionTitle, SubHeader, Content, Filter, OrderFilter, ValueBtns, MinMax, JobList, SearchBar} from "pages/User/styles"
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -10,134 +11,8 @@ import MenuItem from "@material-ui/core/MenuItem";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Input from "@material-ui/core/Input";
-import CardUser from "../components/CardUser";
+import CardUser from "../../components/CardUser";
 import Grow from "@material-ui/core/Grow";
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const MainContent = styled.main`
-  padding-left: 100px;
-`;
-
-const Header = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  & > img {
-    cursor: pointer;
-  }
-
-  & > :nth-child(3) {
-    font-size: 1.5rem;
-  }
-`;
-
-const SectionTitle = styled.h1`
-  font-size: 3rem;
-  color: #e0286799;
-`;
-
-const SubHeader = styled.div`
-  display: flex;
-
-  & > h2 {
-    font-size: 2.25rem;
-    flex: 3;
-
-    & > span {
-      color: #e02867;
-    }
-  }
-`;
-
-const Content = styled.div`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-column-gap: 50px;
-`;
-
-const Filter = styled.div`
-  padding: 20px 30px;
-  border-radius: 12px;
-  grid-column: 1/3;
-  display: flex;
-  flex-direction: column;
-  height: 500px;
-  border: 1px solid black;
-
-  & > * {
-    padding-top: 40px;
-  }
-`;
-
-const OrderFilter = styled.div`
-  display: flex;
-  justify-content: center;
-  font-size: 1.25rem;
-  font-weight: bold;
-
-  & > * {
-    flex: 1;
-  }
-`;
-
-const ValueBtns = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  font-size: 1.25rem;
-  font-weight: bold;
-
-  & > p {
-    padding: 5px 0;
-    margin: 0;
-  }
-
-  & > div {
-    display: flex;
-  }
-
-  & > div :not(:last-child) {
-    margin-right: 10px;
-  }
-`;
-
-const MinMax = styled.div`
-  display: flex;
-
-  & > * {
-    margin: 0 10px;
-  }
-`;
-
-const JobList = styled.div`
-  grid-column: 3/7;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  grid-auto-rows: 300px;
-  grid-gap: 10px;
-  justify-content: center;
-  padding-bottom: 20px;
-`;
-
-const SearchBar = styled.input`
-  align-self: center;
-  background-color: #c4c4c430;
-  padding: 0 10px;
-  border-radius: 30px;
-  border: none;
-  outline: none;
-  box-shadow: 0px 0px 2px gray;
-  height: 65px;
-  flex: 2;
-  font-size: 1.5rem;
-`;
 
 export class User extends Component {
   state = {
